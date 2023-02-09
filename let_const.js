6be47b9882c112d896ffc035de943b58b25454e5
VoidAshfak <1703103@student.ruet.ac.bd>
2023-02-09 12:44:26 +0600

    first lesson learned

---
+++ let_const.js
@@ -0,0 +1,14 @@
+// let hubby = "Mark Davis"
+// console.log(hubby)
+
+// hubby = "Jake Davis"
+// console.log(hubby)
+
+const nums = [1,2,5,7]
+console.log(nums)
+// nums = [1,3,5,7]
+nums.push(9)
+
+console.log(nums)
+
+// Note ==> var is not used anymore. let and const is used. let is a scope variable and can be changed. 
